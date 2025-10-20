4de12193517ac2468ff41ba5381b5230bc9ba5cc
Udie-Cynthia <udiecynthia@gmail.com>
2025-10-20 17:35:27 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+const inCI = !!process.env.CI;
+module.exports = {
+  output: 'standalone',
+  reactStrictMode: true,
+  ...(inCI ? { eslint: { ignoreDuringBuilds: true } } : {}),
+  ...(inCI ? { typescript: { ignoreBuildErrors: true } } : {}),
 };
-module.exports = nextConfig;
